4d0add3b4530e621bbb3b3bc57cba5ad3c3da073
Robert Wiley <ylee@bodhilinux.com>
2025-02-22 15:41:36 -0500

    Normalize gtk theme name

--- build-legacy.sh
+++ build-legacy.sh
@@ -4,6 +4,8 @@
 printf "\n\033[1;33mWARNING:\033[0m For use with Bodhi Linux 7 only.\n\n"
 cd MokshaRedPassion
 sed -i 's/MokshaRedPassion/Icons-Moksha-RedPassion/g' default.edc
+sed -i 's/MokshaRedPassion/MokshaPassion-GTK/g' default.edc
 ./build.sh
 sed -i 's/Icons-Moksha-RedPassion/MokshaRedPassion/g' default.edc
+sed -i 's/MokshaPassion-GTK/MokshaRedPassion/g' default.edc
 cd ..
