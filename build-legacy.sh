a558f6efd2732dd1c7cd279e76759e73151ae29d
Robert Wiley <ylee@bodhilinux.com>
2025-02-22 15:36:06 -0500

    Correct copy paste error in legacy build script

--- build-legacy.sh
+++ build-legacy.sh
@@ -3,7 +3,7 @@
 
 printf "\n\033[1;33mWARNING:\033[0m For use with Bodhi Linux 7 only.\n\n"
 cd MokshaRedPassion
-sed -i 's/MokshaPassion/MokshaSeven-Icons/g' default.edc
+sed -i 's/MokshaRedPassion/Icons-Moksha-RedPassion/g' default.edc
 ./build.sh
-sed -i 's/MokshaSeven-Icons/MokshaPassion/g' default.edc
+sed -i 's/Icons-Moksha-RedPassion/MokshaRedPassion/g' default.edc
 cd ..
